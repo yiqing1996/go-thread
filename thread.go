f5cfaea04dcbaafa1eb14de28c5dd6c68a9a9194
yiqing1996 <75480018+yiqing1996@users.noreply.github.com>
2025-04-30 02:58:35 +0800

    Add files via upload

--- thread.go
+++ thread.go
@@ -16,10 +16,6 @@ type Pool struct {
 	threads map[string]*threadInfo
 }
 
-func Test(a, b int) int {
-	return a / b
-}
-
 func NewThreadPool() *Pool {
 	return &Pool{threads: make(map[string]*threadInfo)}
 }
